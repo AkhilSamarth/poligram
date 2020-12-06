15d684a40ff907947a85e08c0b42891b7c7e6580
EC2 Default User <svp62@njit.edu>
2020-12-06 19:11:29 +0000

    unmocked test for trending news

--- tests/unmocked_unit_tests.py
+++ tests/unmocked_unit_tests.py
@@ -30,6 +30,28 @@ class UnmockedTest(unittest.TestCase):
             self.assertFalse(i["url"] == " ", "False or True")
             self.assertFalse(i["published"] == " ", "False or True")
             self.assertFalse(i["content"] == " ", "False or True")
+            
+    def test_trend_news(self):
+        """ Trend News api unmocked test cases """
+        r_json = (app.trend_news_api_call())
+        
+        if len(r_json) == 0:
+            print("trending thing", r_json)
+            
+        else:
+            assert len(r_json) > 1
+            
+            for i in r_json:
+                self.assertFalse(i["source"] == " ", "False or True")
+                self.assertFalse(i["author"] == " ", "False or True")
+                self.assertFalse(i["title"] == " ", "False or True")
+                self.assertFalse(i["img"] == " ", "False or True")
+                self.assertFalse(i["url"] == " ", "False or True")
+                self.assertFalse(i["published"] == " ", "False or True")
+                self.assertFalse(i["content"] == " ", "False or True")
+            
+        
+        
     
     def test_models(self):
         """ Test instances of models from models.py """
